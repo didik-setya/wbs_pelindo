167c55f479e879ce10ff4e244ccb9a2504efa44b
didik-setiawan <didiksetyaone0@yahoo.com>
2023-06-11 17:31:08 +0700

    add login function member

---
+++ application/controllers/Auth.php
@@ -0,0 +1,83 @@
+<?php
+defined('BASEPATH')or exit('No direct script access allowed');
+class Auth extends CI_Controller {
+    public function index(){
+        $this->load->view('member/auth');
+    }
+
+    public function proccess(){
+
+        $this->form_validation->set_rules('email', 'email', 'required|trim|valid_email');
+        $this->form_validation->set_rules('password', 'password', 'required|trim');
+
+        if($this->form_validation->run() == false){
+            $output = [
+                'type' => 'validation',
+                'err_email' => form_error('email'),
+                'err_pass' => form_error('password'), 
+                'token' => $this->security->get_csrf_hash()
+            ];
+            echo json_encode($output);
+            die;
+        } else {    
+            $this->pv_proccess();
+        }
+
+        
+    }
+
+    private function pv_proccess(){
+        $email = htmlspecialchars($this->input->post('email'));
+        $pass = md5(sha1($this->input->post('password')));
+        $user = $this->db->get_where('member', ['email' => $email])->row();
+
+        if($user){
+            if($user->password == $pass){
+                if($user->is_active == 1){
+
+                    $params = [
+                        'email' => $email,
+                        'status' => $user->is_active,
+                    ];
+                    $output = [
+                        'success' => true,
+                        'msg' => 'Login Success',
+                        'redirect' => base_url('master')
+                    ];
+                    $this->session->set_userdata($params);
+
+                } else if($user->is_active == 2) {
+                    $output = [
+                        'success' => false,
+                        'msg' => 'Email tidak terverifikasi'
+                    ];
+                } else if($user->is_active == 0){
+                    $output = [
+                        'success' => false,
+                        'msg' => 'Akun sudah tidak aktif'
+                    ];
+                } else {
+                    $output = [
+                        'success' => false,
+                        'msg' => 'Unknow user status'
+                    ];
+                }
+            } else {
+                $output = [
+                    'success' => false,
+                    'msg' => 'Password salah'
+                ];
+            }
+        } else {
+            $output = [
+                'success' => false,
+                'msg' => 'Email tidak terdaftar'
+            ];
+        }
+        $output['type'] = 'result';
+        $output['token'] = $this->security->get_csrf_hash();
+
+        echo json_encode($output);
+    }
+
+}
---
+++ application/views/member/auth.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+    <title>Login | Pelindo</title>
+</head>
+<body style="background: #f0f0f0">
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-sm-12 col-md-5 col-lg-4">
+                <div class="text-center mt-5">   
+                    <img src="<?= base_url('assets/web/pelindo.png') ?>" alt="logo-pelindo" width="270px">
+                </div>
+
+                <div class="card mt-3">
+                    <div class="card-body">
+                        <form action="<?= base_url('auth/proccess') ?>" method="post" id="formLogin">
+                            <input type="hidden" name="<?= $this->security->get_csrf_token_name() ?>" value="<?= $this->security->get_csrf_hash() ?>" id="token">
+
+                            <div class="form-group mb-3">
+                                <small>Alamat Email</small>
+                                <input type="text" name="email" id="email" class="form-control">
+                                <small class="text-danger" style="font-size: 10px" id="err_email"></small>
+                            </div>
+                            <div class="form-group mb-3">
+                                <small>Password</small>
+                                <input type="password" name="password" id="password" class="form-control">
+                                <small class="text-danger" style="font-size: 10px" id="err_password"></small>
+                            </div>
+                            <button class="btn btn-sm btn-primary w-100" id="submit">Log In</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "1500",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+        }
+    </script>
+
+    <script>
+        $('#formLogin').submit(function(e){
+            e.preventDefault();
+            let form = $(this).serialize();
+            let url = $(this).attr('action');
+            disabled_form();
+
+            $.ajax({
+                url: url,
+                data: form,
+                type: 'POST',
+                dataType: 'JSON',
+                success: function(d){
+                    $('#token').val(d.token);
+                    undisabled_form();
+           
+
+                    if(d.type == 'validation'){
+                        if(d.err_email == ''){
+                            $('#err_email').html('');
+                        } else {
+                            $('#err_email').html(d.err_email);
+                        }
+                    
+                        if(d.err_pass == ''){
+                            $('#err_password').html('');
+                        } else {
+                            $('#err_password').html(d.err_pass);
+                        }
+                    } else if(d.type == 'result') {
+                        $('#err_email').html('');
+                        $('#err_password').html('');
+
+                        if(d.success == true){
+                            toastr["success"](d.msg, 'Success');
+                            $('#email').val('');
+                            $('#password').val('');
+                            setTimeout(() => {
+                                window.location.href = d.redirect;
+                            }, 1700);
+                        } else {
+                            toastr["error"](d.msg, 'Error');
+                        }
+
+                    }
+                },
+                error: function(xhr){
+                    if(xhr.status === 0){
+                        //no internet connection
+                        toastr["error"]('No internet connection', "Error")
+                    } else if(xhr.status == 403){
+                        //access forbidden
+                        toastr["error"]('Access forbidden', "Error")
+                    } else if(xhr.status == 404){
+                        //page not found
+                        toastr["error"]('Page not found', "Error")
+                    } else if(xhr.status == 500){
+                        //internal server error
+                        toastr["error"]('Internal server error', "Error")
+                    } else {
+                        //unknow error
+                        toastr["error"]('Unknow error', "Error")
+                    }
+                    undisabled_form();
+                }
+            });
+
+        })
+
+        function disabled_form(){
+            let loadingAnimation = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>';
+            $('#submit').html(loadingAnimation)
+            $('#submit').attr('disabled', true);
+            $('#email').attr('disabled', true);
+            $('#password').attr('disabled', true);
+        }
+
+        function undisabled_form(){
+            $('#submit').html('Log In')
+            $('#submit').removeAttr('disabled');
+            $('#email').removeAttr('disabled');
+            $('#password').removeAttr('disabled');        
+        }
+
+    </script>
+</body>
+</html>
